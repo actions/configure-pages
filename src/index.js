8202494c81d2de115aeef0091e969ae6742c6895
Smitha Borkar <12040799+smithaborkar@users.noreply.github.com>
2022-06-07 12:51:34 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,23 @@ const axios = require('axios')
 
 async function getPageBaseUrl() {
   try {
-    if (deployment.requestedDeployment) {
-      const pagesEndpoint = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/pages`
-       const response = await axios.get(
-        pagesEndpoint,
-        {},
-        {
-          headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-            'Content-type': 'application/json'
-          }
+    const pagesEndpoint = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/pages`
+     const response = await axios.get(
+      pagesEndpoint,
+      {},
+      {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          'Content-type': 'application/json'
         }
-      )
-       
-      pageObject = response.data
-      core.info(JSON.stringify(pageObject))
-      core.setOutput('base_url', pageObject.html_url)
-      core.info(`Get the Base URL to the page with endpoint ${pagesEndpoint}`)
-    }
+      }
+    )
+
+    pageObject = response.data
+    core.info(JSON.stringify(pageObject))
+    core.setOutput('base_url', pageObject.html_url)
+    core.info(`Get the Base URL to the page with endpoint ${pagesEndpoint}`)
   } catch (e) {
     console.info('Get on the Page failed', e)
     process.exit(1)
