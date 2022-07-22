86288138eb6bb05d9c25ec23e716fbb1096fa336
James M. Greene <JamesMGreene@github.com>
2022-07-22 18:04:52 -0500

    Fix spacing

--- src/fixtures/nuxt/async.expected.js
+++ src/fixtures/nuxt/async.expected.js
@@ -1,18 +1,17 @@
 const getAllDynamicRoute = async function() {
-    const routes = await (async () => {
-      return ['/posts/hello-world', '/posts/hello-again'];
-    })();
-    return routes;
-  };
-  
-  export default {
-    target: 'static',
-    router: {base: '/docs/'},
-    mode: 'universal',
-    generate: {
-      async routes () {
-        return getAllDynamicRoute();
-      }
+  const routes = await (async () => {
+    return ['/posts/hello-world', '/posts/hello-again'];
+  })();
+  return routes;
+};
+
+export default {
+  target: 'static',
+  router: {base: '/docs/'},
+  mode: 'universal',
+  generate: {
+    async routes () {
+      return getAllDynamicRoute();
     }
-  };
-  
+  }
+};
