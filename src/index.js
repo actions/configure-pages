af945d613334b898a05c62fd584fe4c7e4658827
James M. Greene <JamesMGreene@github.com>
2022-08-03 18:09:46 -0500

    Update index.js to utilize new modules

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const core = require('@actions/core')
 
-const enablePages = require('./enable-pages')
-const getPagesBaseUrl = require('./get-pages-base-url')
-
 // All variables we need from the runtime are loaded here
-const {getContext} = require('./context')
+const { getContext } = require('./context')
+
+const { findOrCreatePagesSite } = require('./api-client')
+const { setPagesPath } = require('./set-pages-path')
+const outputPagesBaseUrl = require('./output-pages-base-url')
 
 async function main() {
   try {
-    const context = getContext()
-    if (context.enablement) {
-      await enablePages(context)
+    const { repositoryNwo, githubToken, enablement, staticSiteGenerator } = getContext()
+
+    const pageObject = await findOrCreatePagesSite({ repositoryNwo, githubToken, enablement })
+    const siteUrl = new URL(pageObject.html_url)
+
+    if (staticSiteGenerator) {
+      setPagesPath({ staticSiteGenerator, path: siteUrl.pathname })
     }
-    await getPagesBaseUrl(context)
+    outputPagesBaseUrl(siteUrl)
   } catch (error) {
     core.setFailed(error)
     process.exit(1)
