dc5b850bfd667948d322f2e8b23329e97e82ad82
James M. Greene <JamesMGreene@github.com>
2022-08-18 17:45:54 -0500

    breaking: Remove trailing slash from base_url and base_path outputs

--- src/output-pages-base-url.js
+++ src/output-pages-base-url.js
@@ -1,10 +1,21 @@
 const core = require('@actions/core')
 
+function removeTrailingSlash(str) {
+  if (str.endsWith('/')) {
+    str = str.slice(0, -1)
+  }
+  return str
+}
+
 function outputPagesBaseUrl(siteUrl) {
-  core.setOutput('base_url', siteUrl.href)
+  // Many static site generators do not want the trailing slash, and it is much easier to add than remove in a workflow
+  const baseUrl = removeTrailingSlash(siteUrl.href)
+  const basePath = removeTrailingSlash(siteUrl.pathname)
+
+  core.setOutput('base_url', baseUrl)
   core.setOutput('origin', siteUrl.origin)
   core.setOutput('host', siteUrl.host)
-  core.setOutput('base_path', siteUrl.pathname)
+  core.setOutput('base_path', basePath)
 }
 
 module.exports = outputPagesBaseUrl
