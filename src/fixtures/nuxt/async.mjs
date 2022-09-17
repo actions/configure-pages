069c984c5e687d4829b955356203d419835b703e
James M. Greene <JamesMGreene@github.com>
2022-09-16 20:37:18 -0500

    Let Prettier reformat all of the things

--- src/fixtures/nuxt/async.mjs
+++ src/fixtures/nuxt/async.mjs
@@ -1,15 +1,15 @@
-const getAllDynamicRoute = async function() {
+const getAllDynamicRoute = async function () {
   const routes = await (async () => {
-    return ['/posts/hello-world', '/posts/hello-again'];
-  })();
-  return routes;
-};
+    return ['/posts/hello-world', '/posts/hello-again']
+  })()
+  return routes
+}
 
 export default {
   mode: 'universal',
   generate: {
-    async routes () {
-      return getAllDynamicRoute();
+    async routes() {
+      return getAllDynamicRoute()
     }
   }
-};
+}
--- src/fixtures/nuxt/default.mjs
+++ src/fixtures/nuxt/default.mjs
@@ -14,9 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
