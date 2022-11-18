51323db1772b3c613b770e70f5b7d9deebbe74d1
James M. Greene <JamesMGreene@github.com>
2022-11-18 00:57:58 -0600

    Fix the Next.js tests

--- src/fixtures/next/default.expected.mjs
+++ src/fixtures/next/default.expected.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: { unoptimized: true },
   experimental: { images: { unoptimized: true } },
   basePath: '/docs',
   reactStrictMode: true,
