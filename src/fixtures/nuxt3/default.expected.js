cfc22b2401766be32af1188f439b6df922f67f70
M.Spinks <mikespinks0401@gmail.com>
2023-03-23 02:43:30 -0500

    + configs in default

--- src/fixtures/nuxt3/default.expected.js
+++ src/fixtures/nuxt3/default.expected.js
@@ -1,6 +1,19 @@
 export default {
   ssr: false,
   app: {
+    head: {
+      title: 'nuxt',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
     base: '/docs/',
     cdnURL: 'https://configure-pages.github.io'
   }
--- src/fixtures/nuxt3/default.js
+++ src/fixtures/nuxt3/default.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+  app: {
+    head: {
+      title: 'nuxt',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  }
+}
